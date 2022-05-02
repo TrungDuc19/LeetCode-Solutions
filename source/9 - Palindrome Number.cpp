44dc306ccc6bdcd145fe268839b77f976eda770e
Cao Trung Đức <103615767+TrungDuc19@users.noreply.github.com>
2022-05-02 23:40:52 +0700

    Create 9 - Palindrome Number.cpp

---
+++ source/9 - Palindrome Number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s1 = to_string(x);
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());
+        return s1 == s2;
+    }
+};
