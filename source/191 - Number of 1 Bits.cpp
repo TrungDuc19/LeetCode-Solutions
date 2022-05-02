c51170909c555496a11edd3c7d5927e4f91b593b
Cao Trung Đức <103615767+TrungDuc19@users.noreply.github.com>
2022-05-02 15:39:04 +0700

    Update 191 - Number of 1 Bits.cpp

--- source/191 - Number of 1 Bits.cpp
+++ source/191 - Number of 1 Bits.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        ios::sync_with_stdio(false); cin.tie(0);
         int cnt = 0;
         for (int i = 0; i < 32; i++) {
             cnt += (n&(1<<i))?1:0;
