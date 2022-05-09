d4aa918fe68ac1b05d25018b3d8ffec42593d36d
Cao Trung Đức <103615767+TrungDuc19@users.noreply.github.com>
2022-05-09 07:27:26 +0700

    Create 1678 - Goal Parser Interpretation.cpp

---
+++ source/1678 - Goal Parser Interpretation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string res = "";
+        for (int i = 0; i < command.size(); i++) {
+            if (command[i] == 'G') {
+                res += 'G';
+            }
+            else if (i < command.size()-1 && command[i] == '(' && command[i+1] == ')') {
+                res += 'o';
+                i++;
+            }
+            else {
+                res += "al";
+                i+=3;
+            }
+        }
+        return res;
+    }
+};
